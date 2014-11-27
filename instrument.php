b7d6c0b8462ea23ea1096314741bee833b04112a
johnmanchester <johnmanchester@college.harvard.edu>
2014-11-27 17:03:26 -0500

    Update instrument.php

--- instrument.php
+++ instrument.php
@@ -1,33 +1,17 @@
-<div>
 <?php
-    $user = query("SELECT * FROM users WHERE id = ?", $_SESSION["id"]);
-    print("<h3> User:  " . $user[0]["username"] . "</h3>");
-?>
-</div>
-<table class = "table table-striped">
-    <tr>
-        <th>Name</th>
-        <th>Instrument</th>
-        <th>Genre</th>
-        <th></th>
-    </tr>
-    <?php
-        
-        foreach ($users as $user)
-        {
-            $user = query("SELECT * FROM users WHERE id = ?", $_SESSION["id"]);
-            print("<tr style='text-align:left;'>");
-            print("<td>" . $user[0]["username"] . "</td>");
-            print("<td>" . $user[0]["instrument"] . "</td>");
-            print("<td>" . $user[0]["genre"] . "</td>");
-            print("</tr>");
-        }
-
-    ?>
-</table>
-<div>
-    <a href="instrument.php">Choose Instrument and Genre</a>
-</div>
-<div>
-    <a href="logout.php">log out</a>
-</div>
+    // configuration
+    require("../includes/config.php"); 
+    //query("SELECT * FROM users WHERE id = ?", $_SESSION["id"]);
+    
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        $instrument = $_POST['instrument'];
+        $genre = $_POST['genre'];
+        //query("SELECT * FROM users WHERE id = ?", $_SESSION['id']);
+        query("INSERT INTO users (id, instrument, genre) VALUES (?, ?, ?)", $_SESSION["id"], $instrument, $genre);
+        redirect("/"); 
+    }
+    else
+    {
+        render("instrumentshow.php");
+    }
